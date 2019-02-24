1f0be61dddc7e5546c43ad6864a26e50aad04475
hungphd <hungphd@iastate.edu>
2019-02-24 01:08:09 -0600

    Update on large scale data

--- src/invocations/RunOnLargeScaleData.java
+++ src/invocations/RunOnLargeScaleData.java
@@ -4,17 +4,25 @@ import java.io.File;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import utils.FileIO;
+import utils.FileUtil;
 import consts.PathConstanct;
 
 public class RunOnLargeScaleData {
 
-	private static final int MYTHREADS = 4;
+	private static final int MYTHREADS = 8;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String inputProjectPath = PathConstanct.PATH_INPUT_IDENTIFER_PROJECT;
 		String outputProjectPath = PathConstanct.PATH_OUTPUT_IDENTIFER_PROJECT;
+		String fpOutputLog=outputProjectPath+File.separator+"alog.txt";
 		ExecutorService executor = Executors.newFixedThreadPool(MYTHREADS);
+		
+		if(!new File(fpOutputLog).isFile()){
+			FileUtil.writeToFile(fpOutputLog, "");
+		}
+		
 
 		File fInput = new File(inputProjectPath);
 		File fOutput = new File(outputProjectPath);
@@ -27,8 +35,9 @@ public class RunOnLargeScaleData {
 				String itemOutputPath = outputProjectPath + File.separator
 						+ arrFilesInput[i].getName() + File.separator;
 				ExtractSequenceForProjectRunnable thread = new ExtractSequenceForProjectRunnable(itemInputPath,
-						itemOutputPath, i);
-				thread.run();
+						itemOutputPath, i,fpOutputLog);
+//				thread.run();
+				executor.execute(thread);
 			}
 		}
 
@@ -38,25 +47,38 @@ public class RunOnLargeScaleData {
 class ExtractSequenceForProjectRunnable implements Runnable {
 	private String inputPath = "", outputPath = "";
 	private int index = 0;
+	private String logPath="";
 
-	ExtractSequenceForProjectRunnable(String inputPath, String outputPath, int index) {
+	ExtractSequenceForProjectRunnable(String inputPath, String outputPath, int index,String logPath) {
 		this.inputPath = inputPath;
 		this.outputPath = outputPath;
 		this.index = index;
+		this.logPath=logPath;
 	}
 
 	@Override
 	public void run() {
-
+		File fIn=new File(inputPath);
 		try {
-			MethodContextSequenceGenerator mcsg = new MethodContextSequenceGenerator(
-					inputPath);
-			mcsg.generateSequences(outputPath);
-			mcsg.generateAlignment(true);
-			System.out.println("Finish success for " + outputPath);
+			
+			File fOut=new File(outputPath);
+			File fSourceOut=new File(outputPath+"source.txt");
+			if(fSourceOut.isFile() && fSourceOut.length()>0){
+				System.out.println(index+"\tAlready for " + outputPath);
+				FileIO.appendStringToFile(index+"\t"+fIn.getName()+"\tDownloaded\n", logPath);
+			} else{
+				MethodContextSequenceGenerator mcsg = new MethodContextSequenceGenerator(
+						inputPath);
+				mcsg.generateSequences(outputPath);
+				mcsg.generateAlignment(true);
+				System.out.println(index+"\tFinish success for " + outputPath);
+				FileIO.appendStringToFile(index+"\t"+fIn.getName()+"\tSuccess\n", logPath);
+			}
+			
 		} catch (Exception ex) {
-			System.out.println("Finish failed for " + outputPath);
+			System.out.println((index+1)+"\tFinish failed for " + outputPath);
 			ex.printStackTrace();
+			FileIO.appendStringToFile((index+1)+"\t"+fIn.getName()+"\tFailed\n", logPath);
 		}
 
 	}

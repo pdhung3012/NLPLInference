0d70c29d8ceaba7a9b32a2d76cdb42935dab4e31
hungphd <hungphd@iastate.edu>
2019-03-04 09:52:48 -0600

    Update alias

--- src/consts/PathConstanct.java
+++ src/consts/PathConstanct.java
@@ -1,43 +1,53 @@
 package consts;
 
 public class PathConstanct {
-	
-public static String PATH_JAVA_CLASSPATH="C:\\Program Files\\Java\\jdk1.8.0_144\\jre\\lib\\rt.jar";
-public static String PATH_INPUT_IDENTIFER_PROJECT="G:\\gitExtractedST2\\";
-public static String PATH_OUTPUT_IDENTIFER_PROJECT="G:\\SeqImplInf\\";
-public static String PATH_COMBINE_TRANS="G:\\CombineTrans\\";
-public static String PATH_COMBINE_SIG_PROJECT="G:\\CombineSigs\\";
-public static String PATH_PROJECT_LOG="G:\\5Repositories\\";
-public static String PATH_PROJECT_TRAIN_TEST_NAME="G:\\projectTrainTestName\\";
-public static String PATH_PROJECT_TTT_DATA="G:\\projectTrainTestData\\";
+
+	public static String PATH_JAVA_CLASSPATH = "C:\\Program Files\\Java\\jdk1.8.0_144\\jre\\lib\\rt.jar";
+	public static String PATH_INPUT_IDENTIFER_PROJECT = "G:\\gitExtractedST2\\";
+	public static String PATH_OUTPUT_IDENTIFER_PROJECT = "G:\\SeqImplInf\\";
+	public static String PATH_COMBINE_TRANS = "G:\\CombineTrans\\";
+	public static String PATH_COMBINE_SIG_PROJECT = "G:\\CombineSigs\\";
+	public static String PATH_PROJECT_LOG = "G:\\5Repositories\\";
+	public static String PATH_PROJECT_TRAIN_TEST_NAME = "G:\\projectTrainTestName\\";
+	public static String PATH_PROJECT_TTT_DATA = "G:\\projectTrainTestData\\";
+	public static String PATH_PROJECT_TTT_ALIAS = "G:\\projectTrainTestAlias\\";
 
 	/*
-	public static String PATH_JAVA_CLASSPATH="/Library/Java/JavaVirtualMachines/jdk1.8.0_141.jdk/Contents/Home/jre/lib/rt.jar";
-	public static String PATH_INPUT_IDENTIFER_PROJECT="/Users/hungphan/git/NLPLTranslation/abc/";
-	public static String PATH_OUTPUT_IDENTIFER_PROJECT="/Users/hungphan/git/NLPLTranslation/sequences/";
-	public static String PATH_COMBINE_TRANS="/Users/hungphan/git/NLPLTranslation/combineTrans/";
-	public static String PATH_COMBINE_SIG_PROJECT="/Users/hungphan/git/NLPLTranslation/combineSig/";
-	public static String PATH_PROJECT_LOG="/Users/hungphan/git/NLPLTranslation/projectLog/";
-	public static String PATH_PROJECT_TRAIN_TEST_NAME="/Users/hungphan/git/NLPLTranslation/projectTrainTestName/";
-	*/
-	
-	public static String fopListLibraryLocation="/Users/hungphan/git/NLPLInference/data/StatTypeProjects/";
-	public static String fopProjectLocation="/Users/hungphan/git/NLPLInference/data/downloaded2/";
-	public static String fopExtractedLocation="/Users/hungphan/git/NLPLInference/data/downloaded3/";
-	
-	public static String fop2ndListLibraryLocation="/Users/hungphan/git/NLPLInference/data/StatTypeProjects/";
-	public static String fop2ndProjectLocation="/Users/hungphan/git/NLPLInference/data/downloaded2/";
-	
-	public static String PATH_DICT_SIGNATURE="/Users/hungphan/git/nlpl/data/dictionary/signatures/";
-	public static String PATH_DICT_DECLARATIONS="/Users/hungphan/git/nlpl/data/dictionary/declarations/";
-	
-//	public static String fop2ndProjectLocation = "G:\\gitHighQualitySpec\\";
-//	public static String fop2ndListLibraryLocation = "G:\\highQualitySpecRepo\\";
-//	
-//	public static String fopProjectLocation = "G:\\gitStatType4\\";
-//	public static String fopExtractedLocation = "G:\\gitExtractedST2\\";
-//	public static String fopListLibraryLocation = "G:\\5Repositories\\";
-//
-//	public static String PATH_DICT_SIGNATURE="/Users/hungphan/git/nlpl/data/dictionary/signatures/";
-//	public static String PATH_DICT_DECLARATIONS="/Users/hungphan/git/nlpl/data/dictionary/declarations/";
+	 * public static String PATH_JAVA_CLASSPATH=
+	 * "/Library/Java/JavaVirtualMachines/jdk1.8.0_141.jdk/Contents/Home/jre/lib/rt.jar"
+	 * ; public static String
+	 * PATH_INPUT_IDENTIFER_PROJECT="/Users/hungphan/git/NLPLTranslation/abc/";
+	 * public static String PATH_OUTPUT_IDENTIFER_PROJECT=
+	 * "/Users/hungphan/git/NLPLTranslation/sequences/"; public static String
+	 * PATH_COMBINE_TRANS="/Users/hungphan/git/NLPLTranslation/combineTrans/";
+	 * public static String
+	 * PATH_COMBINE_SIG_PROJECT="/Users/hungphan/git/NLPLTranslation/combineSig/"
+	 * ; public static String
+	 * PATH_PROJECT_LOG="/Users/hungphan/git/NLPLTranslation/projectLog/";
+	 * public static String PATH_PROJECT_TRAIN_TEST_NAME=
+	 * "/Users/hungphan/git/NLPLTranslation/projectTrainTestName/";
+	 */
+
+	public static String fopListLibraryLocation = "/Users/hungphan/git/NLPLInference/data/StatTypeProjects/";
+	public static String fopProjectLocation = "/Users/hungphan/git/NLPLInference/data/downloaded2/";
+	public static String fopExtractedLocation = "/Users/hungphan/git/NLPLInference/data/downloaded3/";
+
+	public static String fop2ndListLibraryLocation = "/Users/hungphan/git/NLPLInference/data/StatTypeProjects/";
+	public static String fop2ndProjectLocation = "/Users/hungphan/git/NLPLInference/data/downloaded2/";
+
+	public static String PATH_DICT_SIGNATURE = "/Users/hungphan/git/nlpl/data/dictionary/signatures/";
+	public static String PATH_DICT_DECLARATIONS = "/Users/hungphan/git/nlpl/data/dictionary/declarations/";
+
+	// public static String fop2ndProjectLocation = "G:\\gitHighQualitySpec\\";
+	// public static String fop2ndListLibraryLocation =
+	// "G:\\highQualitySpecRepo\\";
+	//
+	// public static String fopProjectLocation = "G:\\gitStatType4\\";
+	// public static String fopExtractedLocation = "G:\\gitExtractedST2\\";
+	// public static String fopListLibraryLocation = "G:\\5Repositories\\";
+	//
+	// public static String
+	// PATH_DICT_SIGNATURE="/Users/hungphan/git/nlpl/data/dictionary/signatures/";
+	// public static String
+	// PATH_DICT_DECLARATIONS="/Users/hungphan/git/nlpl/data/dictionary/declarations/";
 }
---
+++ src/invocations/AliasingParallelCorpusToInt.java
@@ -0,0 +1,104 @@
+package invocations;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+
+import consts.PathConstanct;
+import utils.FileIO;
+
+public class AliasingParallelCorpusToInt {
+
+	public static void addToHashMap(HashMap<String,Integer> mapVocabStrInt,String fpFile){
+		String[] arrContent=FileIO.readStringFromFile(fpFile).trim().split("\n");
+		
+		for(int i=0;i<arrContent.length;i++){
+			String[] arrItems=arrContent[i].trim().split("\\s+");
+			for(int j=0;j<arrItems.length;j++){
+				if(!mapVocabStrInt.containsKey(arrItems[j])){
+					mapVocabStrInt.put(arrItems[j], mapVocabStrInt.size()+1);
+				}
+			}
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String folderInput=PathConstanct.PATH_PROJECT_TTT_DATA;
+		String folderNormalize=PathConstanct.PATH_PROJECT_TTT_ALIAS;
+
+		String fpInTrainSource=folderInput+"train.source.txt";
+		String fpInTrainTarget=folderInput+"train.target.txt";
+		String fpInTuneSource=folderInput+"tune.source.txt";
+		String fpInTuneTarget=folderInput+"tune.target.txt";
+		String fpInTestSource=folderInput+"test.source.txt";
+		String fpInTestTarget=folderInput+"test.target.txt";
+		
+		String fpOutTrainSource=folderNormalize+"train.source.txt";
+		String fpOutTrainTarget=folderNormalize+"train.target.txt";
+		String fpOutTuneSource=folderNormalize+"tune.source.txt";
+		String fpOutTuneTarget=folderNormalize+"tune.target.txt";
+		String fpOutTestSource=folderNormalize+"test.source.txt";
+		String fpOutTestTarget=folderNormalize+"test.target.txt";
+		String fpAlias=folderNormalize+"alias.txt";
+		
+		HashMap<String,Integer> mapVocabStrInt=new LinkedHashMap<>();
+		
+		addToHashMap(mapVocabStrInt, fpInTrainSource);
+		addToHashMap(mapVocabStrInt, fpInTrainTarget);
+		addToHashMap(mapVocabStrInt, fpInTuneSource);
+		addToHashMap(mapVocabStrInt, fpInTuneTarget);
+		addToHashMap(mapVocabStrInt, fpInTestSource);
+		addToHashMap(mapVocabStrInt, fpInTestTarget);
+		
+		System.out.println("Done library of alias "+mapVocabStrInt.size());
+		
+		normalize(fpInTrainSource,fpOutTrainSource,mapVocabStrInt);
+		normalize(fpInTuneSource,fpOutTuneSource,mapVocabStrInt);
+		normalize(fpInTestSource,fpOutTestSource,mapVocabStrInt);
+		
+		StringBuilder sbResult=new StringBuilder();
+		FileIO.writeStringToFile("",fpAlias);
+		int indexLine=0;
+		for(String strItem:mapVocabStrInt.keySet()){
+			indexLine++;
+			sbResult.append(strItem+"\t"+mapVocabStrInt.get(strItem)+"\n");
+			if(indexLine==1000000 || indexLine==mapVocabStrInt.size()){
+				FileIO.appendStringToFile(sbResult.toString(),fpAlias);
+				sbResult=new StringBuilder();
+			}
+		}
+		
+
+	}
+	
+	public static void normalize(String fpInput,String fpOutput,HashMap<String,Integer> mapVocabStrInt){
+		String[] arrTrainS=FileIO.readStringFromFile(fpInput).trim().split("\n");
+		StringBuilder sbResult=new StringBuilder();
+		FileIO.writeStringToFile("", fpOutput);
+		for(int i=0;i<arrTrainS.length;i++){
+			String[] arrItemsS=arrTrainS[i].trim().split("\\s+");
+			StringBuilder strItem=new StringBuilder();
+			
+			for(int j=0;j<arrItemsS.length;j++){
+				if(!mapVocabStrInt.containsKey(arrItemsS[j])){
+					mapVocabStrInt.put(arrItemsS[j], mapVocabStrInt.size()+1);
+				}
+				strItem.append(mapVocabStrInt.get(arrItemsS[j])+" ");
+				
+			}
+			sbResult.append(strItem.toString().trim()+"\n");
+			
+			if(i+1==1000000 || i+1==arrTrainS.length){
+				FileIO.appendStringToFile(sbResult.toString(),fpOutput);
+				sbResult=new StringBuilder();
+			}
+		}
+		
+		
+				
+	}
+
+}

e6e391ab47e057a6ac09d2ff42f6e6d7d7461fd2
hungphd <hungphd@iastate.edu>
2019-03-06 14:43:09 -0600

    ok

---
+++ src/invocations/AnalyseProjectMatchWithStatType.java
@@ -0,0 +1,39 @@
+package invocations;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+import utils.FileIO;
+import consts.PathConstanct;
+
+public class AnalyseProjectMatchWithStatType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fopIdenIfer=PathConstanct.PATH_PROJECT_TRAIN_TEST_NAME;
+		String fopStatType=PathConstanct.PATH_PROJECT_STATTYPE_DATA;
+		
+		String[] allProjectCurrent=FileIO.readStringFromFile(fopIdenIfer+"all.projects.txt").split("\n");
+		String[] arrStatType=FileIO.readStringFromFile(fopStatType+"allProjects.txt").split("\n");
+		int countAppear=0;
+		
+		HashSet<String> setST=new LinkedHashSet<String>();
+		for(int i=0;i<arrStatType.length;i++){
+			setST.add(arrStatType[i]);
+		
+		}
+		StringBuilder sbResult=new StringBuilder();
+		for(int i=0;i<allProjectCurrent.length;i++){
+			boolean exist=setST.contains(allProjectCurrent[i]);
+			if(exist){
+				countAppear++;
+			}
+			sbResult.append(allProjectCurrent[i]+"\t"+exist+"\n");
+		}
+		FileIO.writeStringToFile(sbResult.toString(), fopIdenIfer+"projectStats.txt");
+		System.out.println("count appear: "+countAppear);
+		
+		
+	}
+
+}

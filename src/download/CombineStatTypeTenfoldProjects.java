6ef9f19bd3d9552e67f7f719f0243d32ea64e6e1
hungphd <hungphd@iastate.edu>
2019-03-11 18:48:05 -0500

    update

--- src/download/CombineStatTypeTenfoldProjects.java
+++ src/download/CombineStatTypeTenfoldProjects.java
@@ -13,6 +13,7 @@ public class CombineStatTypeTenfoldProjects {
 		String fpTotalSTProject=PathConstanct.PATH_PROJECT_TRAIN_TEST_NAME+"overlapInST.txt";
 		String fpSupplyProject=PathConstanct.PATH_PROJECT_TRAIN_TEST_NAME+"supplyProjects.txt";
 		String fpRequiredProject=PathConstanct.PATH_PROJECT_TRAIN_TEST_NAME+"requiredProjects.txt";
+		String fpStillMissProject=PathConstanct.PATH_PROJECT_TRAIN_TEST_NAME+"stillMissProjects.txt";
 		
 		String[] arrTotalSTProject=FileIO.readStringFromFile(fpTotalSTProject).split("\n");
 		String[] arrSupplyProject=FileIO.readStringFromFile(fpSupplyProject).split("\n");
@@ -22,17 +23,21 @@ public class CombineStatTypeTenfoldProjects {
 		}
 		
 		String strContent="";
+		String strMiss="";
 		for(int i=0;i<arrTotalSTProject.length;i++){
 			String[] arrItemSt=arrTotalSTProject[i].split("\t");
 			if(arrItemSt[1].equals("false")){
 				if(setSupplyProject.contains(arrItemSt[0])){
 					strContent+=arrItemSt[0]+"\n";
+				} else{
+					strMiss+=arrItemSt[0]+"\n";
 				}
 			} else{
 				strContent+=arrItemSt[0]+"\n";
 			}
 		}
 		FileIO.writeStringToFile(strContent, fpRequiredProject);
+		FileIO.writeStringToFile(strMiss, fpStillMissProject);
 		
 	}
 

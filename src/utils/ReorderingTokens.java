c85df0c92aaf807f791b59421d164a6691632d8d
hungphd <hungphd@iastate.edu>
2019-03-15 13:11:56 -0500

    update

--- src/utils/ReorderingTokens.java
+++ src/utils/ReorderingTokens.java
@@ -62,6 +62,44 @@ public class ReorderingTokens {
 		}
 		FileIO.writeStringToFile(sbResult.toString(), fpOutputTrans2Result);
 	}
+	
+	public static void reorderingTokensOnlyForStatType(String fpInputSource, String fpInputTarget, String fpInputTransResult,
+			String fpOutputTrans2Result) {
+		String[] arrInputSource = FileIO.readStringFromFile(fpInputSource).trim().split("\n");
+		String[] arrInputTrans = FileIO.readStringFromFile(fpInputTransResult).trim().split("\n");
+		StringBuilder sbResult = new StringBuilder();
+		for (int i = 0; i < arrInputSource.length; i++) {
+			String[] arrItemSource = arrInputSource[i].trim().split("\\s+");
+			String[] arrItemTrans = arrInputTrans[i].trim().split("\\s+");
+			String[] arrItemReordered = new String[arrInputTrans.length];
+			for (int j = 0; j < arrItemSource.length; j++) {
+				if (arrItemTrans[j].endsWith(arrItemSource[j])) {
+					arrItemReordered[j] = arrInputTrans[j];
+				} else {
+					// find first occurence of ordered and change position
+					for (int k = j + 1; k < arrItemTrans.length; k++) {
+						if (arrItemTrans[k].endsWith(arrItemSource[j])) {
+							String temp = arrItemTrans[j];
+							arrItemTrans[j] = arrItemTrans[k];
+							arrItemTrans[k] = temp;
+							arrItemReordered[j] = arrInputTrans[j];
+							break;
+						}
+					}
+
+				}
+			}
+
+			String strItemOrdered = "";
+			for (int j = 0; j < arrItemTrans.length; j++) {
+				strItemOrdered += arrItemTrans[j] + " ";
+			}
+			sbResult.append(strItemOrdered.trim() + "\n");
+
+		}
+		FileIO.writeStringToFile(sbResult.toString(), fpOutputTrans2Result);
+	}
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

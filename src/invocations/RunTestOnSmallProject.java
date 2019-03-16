706203cd453f86c8c323fd7b227c07100fcba299
hungphd <hungphd@iastate.edu>
2019-03-16 14:24:52 -0500

    update

--- src/invocations/RunTestOnSmallProject.java
+++ src/invocations/RunTestOnSmallProject.java
@@ -6,10 +6,10 @@ public class RunTestOnSmallProject {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String inPath="/Users/hungphan/Documents/workspace/SampleMethodInvocationProject/";
-		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/SampleMethodInvocationProject/";
-//		String inPath="/Users/hungphan/git/pig/";
-//		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/pig/";
+//		String inPath="/Users/hungphan/Documents/workspace/SampleMethodInvocationProject/";
+//		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/SampleMethodInvocationProject/";
+		String inPath="/Users/hungphan/git/pig/";
+		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/pig/";
 		MethodContextSequenceGenerator mcsg=new MethodContextSequenceGenerator(inPath,arrLibraryPrefix);
 		mcsg.generateSequences(outPath);
 		mcsg.generateAlignment(true);

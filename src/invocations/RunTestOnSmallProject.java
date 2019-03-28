8db2aed6a83cd6fa3bfae4f6a6e3029ea6bd50fa
hungphd <hungphd@iastate.edu>
2019-03-27 23:21:11 -0500

    up

--- src/invocations/RunTestOnSmallProject.java
+++ src/invocations/RunTestOnSmallProject.java
@@ -8,10 +8,12 @@ public class RunTestOnSmallProject {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String inPath="/Users/hungphan/Documents/workspace/SampleMethodInvocationProject/";
-		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/SampleMethodInvocationProject/";
+//		String inPath="/Users/hungphan/Documents/workspace/SampleMethodInvocationProject/";
+//		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/SampleMethodInvocationProject/";
 //		String inPath="/Users/hungphan/git/pig/";
-//		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/pig/";
+//		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/pig/";		
+		String inPath="/Users/hungphan/Documents/workspace/TestExpInference/";
+		String outPath="/Users/hungphan/git/NLPLTranslation/sequences/TestExpInference/";
 		StanfordLemmatizer lemm=new StanfordLemmatizer();
 		MethodContextSequenceGenerator mcsg=new MethodContextSequenceGenerator(inPath,arrLibraryPrefix,lemm);
 		mcsg.generateSequences(outPath);

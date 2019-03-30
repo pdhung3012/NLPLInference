49d7ddfbf8b9b6be7d21cd7bdf26cbd5b9f940db
hungphd <hungphd@iastate.edu>
2019-03-30 03:12:29 -0500

    up

--- src/invocations/GenerateTestingFromProjects.java
+++ src/invocations/GenerateTestingFromProjects.java
@@ -1,5 +1,7 @@
 package invocations;
 
+import java.io.File;
+
 import consts.PathConstanct;
 import utils.StanfordLemmatizer;
 
@@ -8,8 +10,8 @@ public class GenerateTestingFromProjects {
 	public static String[] arrLibraryPrefix={"android","com.google.gwt","com.thoughtworks.xstream","org.hibernate","org.joda.time","java"};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String inPath=PathConstanct.PATH_PROJECT_TTT_TEST_INPUT_PROJECT;
-		String outPath=PathConstanct.PATH_PROJECT_TTT_TEST_IDENTIFIER_PROJECT;
+		String inPath=PathConstanct.PATH_PROJECT_TTT_TEST_INPUT_PROJECT+"TestExpInference"+File.separator;
+		String outPath=PathConstanct.PATH_PROJECT_TTT_TEST_IDENTIFIER_PROJECT+"TestExpInference"+File.separator;
 		StanfordLemmatizer lemm=new StanfordLemmatizer();
 		MethodContextSequenceGenerator mcsg=new MethodContextSequenceGenerator(inPath,arrLibraryPrefix,lemm);
 		mcsg.generateSequences(outPath);

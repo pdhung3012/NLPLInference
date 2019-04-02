ebec9849cf4d6705c735be2084a82403d3b90f16
pdhung3012 <pdhung3012@gmail.com>
2019-04-02 11:46:50 -0500

    up

--- src/consts/PathConstanct.java
+++ src/consts/PathConstanct.java
@@ -12,7 +12,7 @@ public class PathConstanct {
 	public static String PATH_PROJECT_TRAIN_TEST_NAME = "G:\\projectTrainTestName\\";
 	public static String PATH_PROJECT_TTT_DATA = "G:\\projectTrainTestData\\";
 	public static String PATH_PROJECT_TTT_ADDTERMS_DATA = "G:\\projectAddTermData\\";
-	public static String PATH_PROJECT_TTT_CUR_EVAL_DATA = "G:\\projectAddTermData\\v5_allFolds\\b13_sov_fold-1\\";
+	public static String PATH_PROJECT_TTT_CUR_EVAL_DATA = "G:\\projectAddTermData\\v5_allFolds\\b12_tune_fold-1\\";
 	//public static String PATH_PROJECT_TTT_CUR_EVAL_DATA = "G:\\projectTrainTestData\\StatNLPLInfer_v3_allfolds\\b1_sov_fold-1\\";
 	
 	public static String PATH_PROJECT_TTT_ALIAS = "G:\\projectTrainTestAlias\\";

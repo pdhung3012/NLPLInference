4e7317dcec9db75357bbaa127740db79b7b1be21
pdhung3012 <pdhung3012@gmail.com>
2019-10-14 15:10:03 -0500

    update files

--- src/invocations/CombineTrainTuneVersion.java
+++ src/invocations/CombineTrainTuneVersion.java
@@ -1,30 +1,26 @@
 package invocations;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
 
+import consts.PathConstanct;
 import download.ReGenerateAlignment;
 import utils.FileIO;
+import utils.FileUtil;
 
 public class CombineTrainTuneVersion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String fopInput="";
-		String fopTrainTuneRaw="";
-		String fopTrainTuneCombine="";
-		String fopTrainTuneOutput="";
-		
-		String[] arrRawTrainSource=FileIO.readStringFromFile(fopTrainTuneRaw +"train.s").trim().split("\n");
-		String[] arrRawTrainTarget=FileIO.readStringFromFile(fopTrainTuneRaw+"train.t").trim().split("\n");
-		String[] arrCombineTrainSource=FileIO.readStringFromFile(fopTrainTuneCombine+"train.s").trim().split("\n");
-		String[] arrCombineTrainTarget=FileIO.readStringFromFile(fopTrainTuneCombine+"train.t").trim().split("\n");
-		
-		String[] arrRawTuneSource=FileIO.readStringFromFile(fopTrainTuneRaw +"tune.s").trim().split("\n");
-		String[] arrRawTuneTarget=FileIO.readStringFromFile(fopTrainTuneRaw+"tune.t").trim().split("\n");
-		String[] arrCombineTuneSource=FileIO.readStringFromFile(fopTrainTuneCombine+"tune.s").trim().split("\n");
-		String[] arrCombineTuneTarget=FileIO.readStringFromFile(fopTrainTuneCombine+"tune.t").trim().split("\n");
+		String fopInput=PathConstanct.PATH_PROJECT_TTT_CUR_EVAL_DATA;
+		String fopTrainTuneRaw=fopInput+"raw"+File.separator;
+		String fopTrainTuneCombine=fopInput+"context"+File.separator;
+		String fopTrainTuneOutput=fopInput+"outputCombine"+File.separator;
+	
 		
 		
 		PrintStream psNewTrainSource = null, psNewTrainTarget = null,psNewTuneSource = null, psNewTuneTarget = null;
@@ -45,15 +41,28 @@ public class CombineTrainTuneVersion {
 				psNewTuneTarget.close();
 
 			e.printStackTrace();
-			return null;
 		}
 		
-		for(int i=0;i<arrRawTrainSource.length;i++) {
-			psNewTrainSource.println(arrRawTrainSource[i]);
-			psNewTrainSource.println(arrCombineTrainSource[i]);
-			psNewTrainTarget.println(arrRawTrainTarget[i]);
-			psNewTrainTarget.println(arrCombineTrainTarget[i]);
+		
+		ArrayList<String>  arrRawTrainSource=FileUtil.getFileStringArray(fopTrainTuneRaw +"train.s");
+		ArrayList<String>  arrCombineTrainSource=FileUtil.getFileStringArray(fopTrainTuneCombine+"train.s");
+		for(int i=0;i<arrRawTrainSource.size();i++) {
+			psNewTrainSource.println(arrRawTrainSource.get(i));
+			psNewTrainSource.println(arrCombineTrainSource.get(i));
 		}
+		
+		ArrayList<String>  arrRawTrainTarget=FileUtil.getFileStringArray(fopTrainTuneRaw+"train.t");
+		ArrayList<String>  arrCombineTrainTarget=FileUtil.getFileStringArray(fopTrainTuneCombine+"train.t");
+		for(int i=0;i<arrRawTrainTarget.size();i++) {
+			psNewTrainTarget.println(arrRawTrainTarget.get(i));
+			psNewTrainTarget.println(arrCombineTrainTarget.get(i));
+		}
+		
+		String[] arrRawTuneSource=FileIO.readStringFromFile(fopTrainTuneRaw +"tune.s").trim().split("\n");
+		String[] arrRawTuneTarget=FileIO.readStringFromFile(fopTrainTuneRaw+"tune.t").trim().split("\n");
+		String[] arrCombineTuneSource=FileIO.readStringFromFile(fopTrainTuneCombine+"tune.s").trim().split("\n");
+		String[] arrCombineTuneTarget=FileIO.readStringFromFile(fopTrainTuneCombine+"tune.t").trim().split("\n");
+		System.out.println("ok");
 		
 		for(int i=0;i<arrRawTuneSource.length;i++) {
 			psNewTuneSource.println(arrRawTuneSource[i]);

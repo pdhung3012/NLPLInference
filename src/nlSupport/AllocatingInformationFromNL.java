4ad979dde6824541a94c9c29794f30e77c805f88
hungphd <hungphd@iastate.edu>
2020-05-28 00:38:20 -0500

    up

--- src/nlSupport/AllocatingInformationFromNL.java
+++ src/nlSupport/AllocatingInformationFromNL.java
@@ -37,7 +37,7 @@ public class AllocatingInformationFromNL {
 		
 	}
 	
-	public static Object[] getVarAppearInNLDescription(String varInCode,String nlDescription,HashMap<String,String> mapStringLiterals,) {
+	public static Object[] getVarAppearInNLDescription(String varInCode,String nlDescription,HashMap<String,String> mapStringLiterals) {
 		StringBuilder sb=new StringBuilder();
 		String[] arrVarsInNL=nlDescription.split("\\s+");
 		String[] arrVarsInCode=varInCode.split(",");

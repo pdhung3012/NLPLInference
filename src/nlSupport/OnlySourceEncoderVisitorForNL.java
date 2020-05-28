ab1e58f3ce2ac84d4064c8dc28be27f78d0606e9
hungphd <hungphd@iastate.edu>
2020-05-28 02:08:02 -0500

    update

--- src/nlSupport/OnlySourceEncoderVisitorForNL.java
+++ src/nlSupport/OnlySourceEncoderVisitorForNL.java
@@ -1449,6 +1449,10 @@ public class OnlySourceEncoderVisitorForNL extends ASTVisitor {
 						return false;
 //					this.fullTokens.append(" " + getQualifiedName(tb) + "#var ");
 					this.partialTokens.append(" " + getName(tb) + "#var ");
+					LocalEntity le = new LocalEntity();
+					le.setStrCodeReprensent(b.getName());
+					le.setStrTypeOfEntity(getName(tb));
+					setLocalVariables.add(le);
 				}
 			} else if (b instanceof ITypeBinding) {
 				ITypeBinding tb = (ITypeBinding) b;

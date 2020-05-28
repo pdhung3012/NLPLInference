0ff2a205cda6029b6e56a30b13cfc7e44cffb093
hungphd <hungphd@iastate.edu>
2020-05-27 23:05:29 -0500

    fix incorrect terms in sequence

--- src/nlSupport/AllocatingInformationFromNL.java
+++ src/nlSupport/AllocatingInformationFromNL.java
@@ -2,6 +2,9 @@ package nlSupport;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 
 import consts.PathConstanct;
@@ -34,17 +37,30 @@ public class AllocatingInformationFromNL {
 		
 	}
 	
-	public static String getVarAppearInNLDescription(String varInCode,String nlDescription) {
+	public static String getVarAppearInNLDescription(String varInCode,String nlDescription,HashSet<String> setVarsAndAbstractInfo,HashMap<String,String> mapStringLiterals,LinkedHashMap<String,String> mapTotalVarAndTypes) {
 		StringBuilder sb=new StringBuilder();
 		String[] arrVarsInNL=nlDescription.split("\\s+");
 		String[] arrVarsInCode=varInCode.split(",");
 		LinkedHashSet<String> setNLTokens=new LinkedHashSet<String>();
+		
+		mapTotalVarAndTypes=new LinkedHashMap<String, String>();
 		for(int i=0;i<arrVarsInNL.length;i++) {
 			setNLTokens.add(arrVarsInNL[i]);
 			if(isNumeric(arrVarsInNL[i])) {
-				sb.append(arrVarsInNL[i]+"\t"+"int"+"\n");
-			} else if(arrVarsInNL[i].startsWith("\"") && arrVarsInNL[i].endsWith("\"") ) {
-				sb.append(arrVarsInNL[i]+"\t"+"String"+"\n");
+				if(arrVarsInNL[i].contains(".")) {
+					mapTotalVarAndTypes.put(arrVarsInNL[i], "double");
+					setVarsAndAbstractInfo.add(arrVarsInNL[i]);
+				} else {
+					mapTotalVarAndTypes.put(arrVarsInNL[i], "int");
+					setVarsAndAbstractInfo.add(arrVarsInNL[i]);
+				}
+//				sb.append(arrVarsInNL[i]+"\t"+"int"+"\n");
+			} 
+			else if(arrVarsInNL[i].startsWith("StringLit_")) {
+//				sb.append(arrVarsInNL[i]+"\t"+"String"+"\n");
+				setVarsAndAbstractInfo.add(arrVarsInNL[i]);
+				String strValue=mapStringLiterals.get(arrVarsInNL[i]);
+				mapTotalVarAndTypes.put(strValue, "String");
 			}
 		}
 		
@@ -55,18 +71,27 @@ public class AllocatingInformationFromNL {
 				continue;
 			}
 			if(setNLTokens.contains(itemVarAndType[0])) {
-				sb.append(itemVarAndType[0]+"\t"+itemVarAndType[1]+"\n");
+//				sb.append(itemVarAndType[0]+"\t"+itemVarAndType[1]+"\n");
+				setVarsAndAbstractInfo.add(itemVarAndType[0]);
+				mapTotalVarAndTypes.put(itemVarAndType[0], itemVarAndType[1]);
 			}
+		}
+		sb=new StringBuilder();
+		for(String key:mapTotalVarAndTypes.keySet()) {
+			sb.append(key+"\t"+mapTotalVarAndTypes.get(key)+"\n");
 		}
 		return sb.toString();
 	}
 	public static String regexCamelCase="(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])";
-	public static String getTokenInformation(String nlDescription,StanfordLemmatizer lemm) {
+	public static String getTokenInformation(String nlDescription,HashSet<String> setVarsAndAbss,StanfordLemmatizer lemm) {
 		StringBuilder sbTokens=new StringBuilder();
 		String[] arrInputTokens=nlDescription.split("\\s+");
 		for(int i=0;i<arrInputTokens.length;i++) {
 			String[] arrItems=arrInputTokens[i].split(regexCamelCase);
 			for(int j=0;j<arrItems.length;j++) {
+				if(setVarsAndAbss.contains(arrItems[j])) {
+					continue;
+				}
 				sbTokens.append(arrItems[j]+" ");
 			}
 		}
@@ -89,6 +114,42 @@ public class AllocatingInformationFromNL {
 	        return false;
 	    }
 	    return true;
+	}
+	
+	public static String abstractStringLiteralAndNumberLiteral(String input,HashMap<String,String> mapCodeEntities) {
+		StringBuilder sbItem=new StringBuilder();
+		StringBuilder sbTotal=new StringBuilder();
+		int indexQuote=0;
+		boolean isInQuoteMode=false;
+		mapCodeEntities=new HashMap<String,String>();
+		for(int i=0;i<input.length();i++) {
+			char cIndex=input.charAt(i);
+			if(cIndex=='\"') {
+				indexQuote++;
+				if(indexQuote%2==1) {
+					sbItem=new StringBuilder();
+					sbItem.append(cIndex);
+					isInQuoteMode=true;
+				}else {
+					isInQuoteMode=false;
+					sbItem.append(cIndex);
+					int numberIndex=indexQuote%2;
+					String key="StringLit_"+numberIndex;
+					sbTotal.append(" "+key+" ");
+					mapCodeEntities.put(key, sbItem.toString());
+					
+				}
+				
+			} else {
+				if(!isInQuoteMode) {
+					sbTotal.append(cIndex);
+				}else {
+					sbItem.append(cIndex);
+				}
+			}
+			
+		}
+		return sbTotal.toString();
 	}
 
 	public static void main(String[] args) {
@@ -123,6 +184,8 @@ public class AllocatingInformationFromNL {
 			new File(fopOutputElement).mkdir();
 			String strCodeToken=arrSourceContent[i-1].split(splitContent)[0];
 			String strVarInfoInCode=arrSourceContent[i-1].split(splitContent)[1];
+			
+			
 			String[] arrTokens=strCodeToken.split("\\s+");
 			PrePostFixObject ppfx=getPrepostfixOfUnkToken(arrTokens, unknownMethodToken);
 			FileIO.writeStringToFile(ppfx.getPrefix(), fopOutputElement+fname_Prefix);
@@ -130,15 +193,22 @@ public class AllocatingInformationFromNL {
 			String nlDescription=FileIO.readStringFromFile(fopInputTextMetaData+nameOfFile+".java").split("\n")[1];
 //			System.out.println(nlDescription);
 			nlDescription=nlDescription.replaceAll(","," ").replaceAll("\\("," ").replaceAll("\\)"," ").replaceAll("\\."," ");
+			
+			HashMap<String,String> mapLiterals=new HashMap<String, String>();
+			String nlDesAfterNormalize=abstractStringLiteralAndNumberLiteral(nlDescription, mapLiterals);
 //			System.out.println(nlDescription);
-			String listVariablesInNLAndType=getVarAppearInNLDescription(strVarInfoInCode,nlDescription);
+			LinkedHashMap<String,String> mapTotalVarAndTypes=new LinkedHashMap<String, String>();
+			LinkedHashSet<String> setVarAndAbs=new LinkedHashSet<String>();
+			String listVariablesInNLAndType=getVarAppearInNLDescription(strVarInfoInCode,nlDesAfterNormalize,setVarAndAbs,mapLiterals,mapTotalVarAndTypes);
 			FileIO.writeStringToFile(listVariablesInNLAndType, fopOutputElement+fname_varInNaturalLanguage);
-			String nlTokens=getTokenInformation(nlDescription,lemm);
+			String nlTokens=getTokenInformation(nlDesAfterNormalize,setVarAndAbs,lemm);
 			FileIO.writeStringToFile(nlTokens, fopOutputElement+fname_lemmTokenInNL);			
 //			String prefix
 		}
 		
 		
 	}
+	
+	
 
 }
